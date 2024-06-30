a7a394896a2a74f6c79702b63d4e6d5f685fa3f2
YevheniiaYakymenko <152288008+YevheniiaYakymenko@users.noreply.github.com>
2024-06-30 20:08:59 +0200

    localSt

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,21 +3,23 @@ import SearchBox from "../SearchBox/SearchBox";
 import ContactList from "../ContactList/ContactList";
 import ContactForm from "../ContactForm/ContactForm";
 
-export default function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+const initialContacts = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
 
+export default function App() {
+  const [contacts, setContacts] = useState(initialContacts);
   const [filter, setFilter] = useState("");
 
   useEffect(() => {
-    const contactList = localStorage.getItem("contacts");
-    if (contactList) {
-      const bookContacts = JSON.parse(contactList);
-      setContacts(bookContacts);
+    const storedContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (storedContacts) {
+      setContacts(storedContacts);
+    } else {
+      localStorage.setItem("contacts", JSON.stringify(initialContacts));
     }
   }, []);
 
@@ -30,17 +32,17 @@ export default function App() {
       return prevContacts.filter((contact) => contact.id !== id);
     });
   };
+
   const handleFilterChange = (searchValue) => {
     setFilter(searchValue);
   };
+
   const filteredContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 
   const addContact = (newContact) => {
-    setContacts((prevContacts) => {
-      return [...prevContacts, newContact];
-    });
+    setContacts((prevContacts) => [...prevContacts, newContact]);
   };
 
   return (
